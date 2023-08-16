f737a84ac2c9903c22ecf88428bbe6d520db7ef8
Lonndy007 <133030964+Lonndy007@users.noreply.github.com>
2023-08-16 12:35:37 +0300

    Delete NewsPaper directory

--- NewsPaper/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import  Postivew
-
-
-urlpatterns = [
-   path('',  Postivew.as_view()),
-]
--- NewsPaper/news/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView
-from .models import Post
-
-
-class Postivew(ListView):
-    model = Post
-    ordering = 'name'
-    template_name = 'news.html'
-    context_object_name = 'news'
