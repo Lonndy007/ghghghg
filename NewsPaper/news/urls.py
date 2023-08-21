ff218596619f3d2a327de49ee7f521c0b6b06319
Lonndy007 <133030964+Lonndy007@users.noreply.github.com>
2023-08-21 19:09:00 +0300

    Delete NewsPaper directory

--- NewsPaper/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import  Postivew,PostDetail,PostCreate
-
-
-urlpatterns = [
-   path('',  Postivew.as_view()),
-   path('<int:pk>', PostDetail.as_view()),
-   path('create/', PostCreate.as_view(), name='news_create'),
-]
